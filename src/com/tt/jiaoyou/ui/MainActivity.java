37b61698528c06b935523179c2862f565580bf26
糖哥 <752387519@qq.com>
2015-05-13 23:19:22 +0800

    退出功能

--- src/com/tt/jiaoyou/ui/MainActivity.java
+++ src/com/tt/jiaoyou/ui/MainActivity.java
@@ -1,13 +1,15 @@
 package com.tt.jiaoyou.ui;
 
 
+import android.app.AlertDialog;
 import android.app.TabActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.widget.RadioButton;
+import android.view.KeyEvent;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TabHost;
@@ -82,6 +84,46 @@ public class MainActivity extends TabActivity implements OnCheckedChangeListener
 		}
 	};
 
+	@Override  
+    public boolean dispatchKeyEvent(KeyEvent event)  
+    {  
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK )  
+        {  
+        	if (event.getAction() == KeyEvent.ACTION_DOWN && event.getRepeatCount() == 0) {   
+        		// 创建退出对话框  
+                AlertDialog isExit = new AlertDialog.Builder(this).create();  
+                // 设置对话框标题  
+                isExit.setTitle("确定要退出吗");  
+                // 设置对话框消息  
+                isExit.setMessage(getString(R.string.str_exit_text1));  
+                // 添加选择按钮并注册监听  
+                isExit.setButton("确定", listener);  
+                isExit.setButton2("取消", listener);  
+                // 显示对话框  
+                isExit.show();  
+           }  
+            return true;  
+        }  
+          
+        return super.dispatchKeyEvent(event);
+    } 
 	
+	/**监听对话框里面的button点击事件*/  
+    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener()  
+    {  
+        public void onClick(DialogInterface dialog, int which)  
+        {  
+            switch (which)  
+            {  
+            case AlertDialog.BUTTON_POSITIVE:// "确认"按钮退出程序  
+                finish();  
+                break;  
+            case AlertDialog.BUTTON_NEGATIVE:// "取消"第二个按钮取消对话框  
+                break;  
+            default:  
+                break;  
+            }  
+        }  
+    };    
 
 	}
